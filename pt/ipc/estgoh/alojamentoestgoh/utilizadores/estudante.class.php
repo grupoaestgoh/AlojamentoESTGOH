b7e8dd28661ca22981ec9734091934c0b20d5c78
piscasilva <pisca.silva.31@gmail.com>
2017-12-21 01:40:49 +0000

    Commit arranjos
    No gestor, anunciante e estudante

--- pt/ipc/estgoh/alojamentoestgoh/utilizadores/estudante.class.php
+++ pt/ipc/estgoh/alojamentoestgoh/utilizadores/estudante.class.php
@@ -1,13 +1,10 @@
 <?php
 class Estudante extends Utilizador{
-  //Atributos de um estudante
-  public $Estado;
 
   //Construtor da classe estudante
-  function __construct($Id_Utilizador,$Nome,$Email,$Password,$Tipo,$Data_Inscricao,$Estado)
+  function __construct($Id_Utilizador,$Nome,$Email,$Password,$Tipo,$Data_Inscricao)
   {
     parent::__construct($Id_Utilizador,$Nome,$Email,$Password,$Tipo,$Data_Inscricao);
-    $this->Estado=$Estado;
   }
 }
  ?>
--- pt/ipc/estgoh/alojamentoestgoh/utilizadores/gestor.class.php
+++ pt/ipc/estgoh/alojamentoestgoh/utilizadores/gestor.class.php
@@ -1,20 +1,23 @@
 <?php
 class Gestor extends Utilizador{
-
+  //Atributos de um gestor
+  public $Estado;
+  
   //Construtor da classe gestor
-  function __construct($Id_Utilizador,$Nome,$Email,$Password,$Tipo,$Data_Inscricao)
+  function __construct($Id_Utilizador,$Nome,$Email,$Password,$Tipo,$Data_Inscricao,$estado)
   {
     parent::__construct($Id_Utilizador,$Nome,$Email,$Password,$Tipo,$Data_Inscricao);
+    $this->Estado=$Estado;
   }
   //Devolve gestor em array sem atributo id
   public function to_array_sem_id()
   {
-    return array("n"=>$this->Nome,"e"=>$this->Email,"p"=>$this->Password,"t"=>$this->$Tipo,"d"=$this->Data_Inscricao)
+    return array("n"=>$this->Nome,"e"=>$this->Email,"p"=>$this->Password,"t"=>$this->$Tipo,"d"=$this->Data_Inscricao,"es"=$this->Estado)
   }
   //Devolve gestor em array sem atributo id
   public function to_array_com_id()
   {
-    return array("i"=>$this->Id_Utilizador,"n"=>$this->Nome,"e"=>$this->Email,"p"=>$this->Password,"t"=>$this->$Tipo,"d"=$this->Data_Inscricao)
+    return array("i"=>$this->Id_Utilizador,"n"=>$this->Nome,"e"=>$this->Email,"p"=>$this->Password,"t"=>$this->$Tipo,"d"=$this->Data_Inscricao,"es"=$this->Estado)
   }
 }
  ?>
