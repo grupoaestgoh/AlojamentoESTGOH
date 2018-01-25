6e7b2e00c8c579f118acda614cfafbb0fdbc50b9
Prolicide <fabioloureiro91@live.com.pt>
2018-01-25 21:17:38 +0000

    Pagina_Finalizada
    Registar_outros_gestores

--- pt/ipc/estgoh/alojamentoestgoh/registo_outros_gestores.php
+++ pt/ipc/estgoh/alojamentoestgoh/registo_outros_gestores.php
@@ -10,7 +10,7 @@
 ?>
 
 <!-- Navigation -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top" id="mainNav">
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top navGestor" id="mainNav">
  <div class="container">
    <a class="navbar-brand"  href="AnunciosGestor.html" ><font  size="6" color="white">AlojamentoESTGOH</font></a>
    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
@@ -22,55 +22,55 @@
      <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Components">
        <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseComponents" data-parent="#exampleAccordion">
          <i class="fa fa-fw fa-wrench"></i>
-         <span class="nav-link-text">Meus Anúncios</span>
+         <span class="nav-link-text"><?php print $MeusAnu ?></span>
        </a>
        <ul class="sidenav-second-level collapse" id="collapseComponents">
          <li>
-           <a href="InserirAnuncios.html">Inserir Anúncio</a>
+           <a href="InserirAnuncios.html"><?php print $InsereAnu ?></a>
          </li>
          <li>
-           <a href="AnunciosGestor.html">Ver Meus Anúncios</a>
+           <a href="AnunciosGestor.html"><?php print $VerMeusAnu ?></a>
          </li>
        </ul>
      </li>
      <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Example Pages">
        <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseExamplePages" data-parent="#exampleAccordion">
          <i class="fa fa-fw fa-file"></i>
-         <span class="nav-link-text">Anúncios dos proprietários</span>
+         <span class="nav-link-text"><?php print $AnuProprietarios ?></span>
        </a>
        <ul class="sidenav-second-level collapse" id="collapseExamplePages">
          <li>
-           <a href="AnunciosNovosPendentes.html">Anúncios novos pendentes</a>
+           <a href="AnunciosNovosPendentes.html"><?php print $AnuNovosPend ?></a>
          </li>
          <li>
-           <a href="AnunciosEditadosPendentes.html">Anúncios editados pendentes</a>
+           <a href="AnunciosEditadosPendentes.html"><?php print $AnuEditPend ?></a>
          </li>
          <li>
-           <a href="AnunciosProprietarios.html">Ver Anúncios dos proprietários</a>
+           <a href="AnunciosProprietarios.html"><?php print $VerAnuProprietarios ?></a>
          </li>
        </ul>
      </li>
      <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Menu Levels">
        <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseMulti" data-parent="#exampleAccordion">
          <i class="fa fa-fw fa-sitemap"></i>
-         <span class="nav-link-text">Utilizadores</span>
+         <span class="nav-link-text"><?php print $Uti ?></span>
        </a>
        <ul class="sidenav-second-level collapse" id="collapseMulti">
          <li>
-           <a href="registoGestor.html">Registar gestor</a>
+           <a href="registoGestor.html"><?php print $RegGestor ?></a>
          </li>
          <li>
-           <a href="desativaProprietario.html">Desativar proprietário</a>
+           <a href="desativaProprietario.html"><?php print $DesaProprietario ?></a>
          </li>
          <li>
-           <a href="desativaGestor.html">Desativar gestor</a>
+           <a href="desativaGestor.html"><?php print $DesaGestor ?></a>
          </li>
        </ul>
      </li>
      <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Example Pages">
        <a class="nav-link "  href="AnunciosDenunciados.html" >
          <i class="fa fa-fw fa-file"></i>
-         <span >Anúncios denunciados</span>
+         <span ><?php print $AnunDenunciados ?></span>
        </a>
      </li>
    </ul>
@@ -84,9 +84,9 @@
    <ul class="navbar-nav ml-auto">
 
      <li class="nav-item dropdown">
-       <a class="nav-link dropdown-toggle mr-lg-2" id="alertsDropdown" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+       <a class="nav-link dropdown-toggle mr-lg-2 corBranca" id="alertsDropdown" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
          <i class="fa fa-fw fa-bell"></i>
-         <span class="d-lg-none">Notificações
+         <span class="d-lg-none"><?php print $Notificacao ?>
            <span class="badge badge-pill badge-warning">6 New</span>
          </span>
          <span class="indicator text-warning d-none d-lg-block">
@@ -94,166 +94,28 @@
          </span>
        </a>
        <div class="dropdown-menu notificacoes" aria-labelledby="alertsDropdown">
-         <h6 class="dropdown-header">Novas Notificações:</h6>
-         <div class="dropdown-divider"></div>
-         <a class="dropdown-item" href="#">
-           <span class="text-success">
-             <strong>
-               <i class="fa"></i>Tem anuncio para aprovação</strong>
-           </span>
-           <span class="small float-right text-muted">11:21 horas</span>
-           <div class="dropdown-message small">Tem anuncio editado para aprovar.</div>
-         </a>
-         <div class="dropdown-divider"></div>
-         <a class="dropdown-item" href="#">
-           <span class="text-warning">
-             <strong>
-               <i class="fa"></i>Tem pedido de desativação</strong>
-           </span>
-           <span class="small float-right text-muted">11:21 horas</span>
-           <div class="dropdown-message small">O utilizador Manuel pediu para desativar a conta.</div>
-         </a>
-         <div class="dropdown-divider"></div>
-         <a class="dropdown-item" href="#">
-           <span class="text-success">
-             <strong>
-               <i class="fa"></i>Tem anuncio para aprovação</strong>
-           </span>
-           <span class="small float-right text-muted">11:21 horas</span>
-           <div class="dropdown-message small">Tem anuncio editado para aprovar.</div>
-         </a>
-         <div class="dropdown-divider"></div>
-
-         <a class="dropdown-item" href="#">
-           <span class="text-success">
-             <strong>
-               <i class="fa"></i>Tem anuncio para aprovação</strong>
-           </span>
-           <span class="small float-right text-muted">11:21 horas</span>
-           <div class="dropdown-message small">Tem anuncio editado para aprovar.</div>
-         </a>
-         <div class="dropdown-divider"></div>
-
-         <a class="dropdown-item" href="#">
-           <span class="text-warning">
-             <strong>
-               <i class="fa"></i>Tem pedido de desativação</strong>
-           </span>
-           <span class="small float-right text-muted">11:21 horas</span>
-           <div class="dropdown-message small">O utilizador Manuel pediu para desativar a conta.</div>
-         </a>
+				 <!-- Notificações -->
        </div>
      </li>
      <li class="nav-item dropdown">
-       <a class="nav-link dropdown-toggle mr-lg-2" id="alertsDropdown" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+       <a class="nav-link dropdown-toggle mr-lg-2 corBranca" id="alertsDropdown" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
          <i class="fa fa-user" aria-hidden="true"></i>
-         <span class="d-lg-none">Meus Dados
+         <span class="d-lg-none"><?php print $MeusDados1 ?>
            <span class="badge badge-pill badge-warning">6 New</span>
          </span>
-
        </a>
        <div class="dropdown-menu " aria-labelledby="alertsDropdown">
-         <h6 class="dropdown-header">Meus Dados:</h6>
-         <div class="dropdown-divider"></div>
-
-         <a class="dropdown-item" href="#">
-          <strong> Nome</strong>
-           <div class="dropdown-message small">António Joaquim</div>
-         </a>
-
-         <div class="dropdown-divider"></div>
-
-         <a class="dropdown-item" href="#">
-          <strong> Email</strong>
-           <div class="dropdown-message small">antoniojoaquim@gmail.com</div>
-         </a>
-
-         <div class="dropdown-divider"></div>
-
-         <a class="dropdown-item" href="#">
-           <strong>
-             <i class="fa"></i>
-             <button type="button" class="nav-link especialBotao"  data-toggle="modal" data-target="#myModal20"><font  size="4" color="black">Alterar Dados</font></button>
-           </strong>
-
-         </a>
+				 <!-- Dados -->
        </div>
      </li>
      <li class="nav-item">
-       <a class="nav-link" data-toggle="modal" data-target="#exampleModal">
-         <i class="fa fa-fw fa-sign-out"></i>Terminar Sessão</a>
+       <a class="nav-link corBranca" data-toggle="modal" data-target="#exampleModal">
+         <i class="fa fa-fw fa-sign-out"></i><?php print $terminaSessao ?></a>
      </li>
    </ul>
  </div>
 </nav>
 
-<div id="myModal20" class="modal fade" role="dialog">
-  <!-- Modal ELiminar -->
-  <div class="modal fade" id="myModalDesativar" role="dialog">
-    <div class="modal-dialog">
-  <!-- Modal content-->
-  <div class="modal-content">
-    <div class="modal-header">
-      <button type="button" class="close" data-dismiss="modal">&times;</button>
-      <h4 class="modal-title">Desactivar Conta</h4>
-    </div>
-    <div class="modal-body aumenta">
-      <p>Tem a certeza que quer desativar a conta?</p>
-    </div>
-    <div class="modal-footer">
-      <button type="button" class="btn btn-danger" data-dismiss="modal">Desativar</button>
-      <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
-    </div>
-  </div>
-</div>
-</div>
-<!-- Fim ELiminar -->
-
-<div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h4 class="modal-title">Alterar Password</h4>
-      </div>
-      <div class="modal-body">
-        <form action="#">
-          <br>
-            <label><b>Password nova:</b></label><br>
-            <input class="form-control" type="email" placeholder="Password" name="eml" data-toggle="tooltip" title="Se quer alterar o email insira um novo e edite!Caso contrario deixe o inicial!">
-            <br>
-            <label><b>Repetir Password Nova:</b></label><br>
-            <input class="form-control" type="password" placeholder="Repetir password" name="psw" data-toggle="tooltip" title="Se quer alterar a password insira uma nova e edite!">
-            <br>
-            <div id="Aviso1" class="alert alert-success" role="alert" style="display:none;" >
-                <div class="row leftCaracteris">
-                <div class="col-lg-2 ">
-                <img class="alertaImg" src="img/certo.png" alt="">
-                </div>
-                <div class="col-lg-8">
-                  <span class="glyphicon glyphicon-alert">Alterada com sucesso!</span>
-                </div>
-              </div>
-            </div>
-            <div class="row" style="text-align:center;">
-            <div class="col-12">
-            <button class="btn btn-primary navbar-btn" onclick="Mudarestado('Aviso1')" type="submit" >Editar</button>
-            </div>
-           </div>
-        </form>
-      </div>
-      <div class="modal-footer">
-        <div class="row">
-          <div class="col-6">
-              <button type="button" class=" btn btn-danger"  data-toggle="modal" data-target="#myModalDesativar"><font  size="3" color="white">Desativar Conta</font></button>
-          </div>
-          <div class="col-6">
-            <button type="button" class="btn btn-default corPreta" data-dismiss="modal">Fechar</button>
-          </div>
-        </div>
-      </div>
-    </div>
-
-  </div>
-</div>
 
 <!-- Page Content -->
   <div class="row">
@@ -459,7 +321,7 @@ if(isset($_POST["registar"]) && !empty($_POST["registar"])){
 	}
 
   $conteudo_principal = ob_get_contents();
-	$rodape=true;
+	$rodape=false;
   ob_end_clean();
   //master page
   include($layout);
